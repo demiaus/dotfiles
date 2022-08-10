314abad373dfc3da6e4dd0b9c794292170eb0a73
krumeluu <krumeluu@duck.com>
2022-08-10 20:46:27 +0300

    Change sound effects and notification format

--- scripts/vol.sh
+++ scripts/vol.sh
@@ -2,23 +2,25 @@
 
 if [ ! $# = 1 ]; then
         echo "Usage:"
-        echo "vol.sh <up|down|mute>"
+        echo "$0 <up|down|mute>"
         exit 1;
 fi
 
 case $1 in
 'up')
-        pamixer -i 5 ;;
+        pamixer -i 5
+        canberra-gtk-play -i device-added   -d "changeVolume" > /dev/null ;;
 'down')
-        pamixer -d 5 ;;
+        pamixer -d 5
+        canberra-gtk-play -i device-removed -d "changeVolume" > /dev/null ;;
 'mute')
+        canberra-gtk-play -i device-removed -d "changeVolume" > /dev/null
         pamixer -t ;;
 *)
         echo "parsing error"
         exit 1 ;;
 esac
 
-dunstify -r 69 -u low $(pamixer --get-volume-human)
-canberra-gtk-play -i audio-volume-change -d "changeVolume"
+dunstify -r 69 -u low "$(pamixer --get-volume) %"
 
 exit 0
