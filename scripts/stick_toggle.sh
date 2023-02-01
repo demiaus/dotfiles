6aa3641a9616c6ba2da6885c60147721edcc0cd3
demiaus <demiaus@amideus.fi>
2023-02-01 18:08:09 +0200

    Clean scripts

--- scripts/stick_toggle.sh
+++ scripts/stick_toggle.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ID=$(xinput | grep -i "Touchpad" | cut -f 2 | cut -d "=" -f 2)
+ID=$(xinput | grep -i "Stick" | cut -f 2 | cut -d "=" -f 2)
 
 if [ $(xinput list-props $ID | grep "Device Enabled" | cut -f 3) -eq "1" ]; then
 	xinput --disable $ID
